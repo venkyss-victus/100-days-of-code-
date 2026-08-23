ea7ef5d4c54d4b67a3d3a5e69e73e347d7c1fe0c
VENKY SHARMA <venkycamb@gmail.com>
2026-08-23 15:04:01 +0530

    add Day3-6 c program

---
+++ Day3-6.c
@@ -0,0 +1,16 @@
+//Write a program to swap two numbers using a third variable.
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    scanf("%d %d", &a, &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swap: %d %d", a, b);
+
+    return 0;
+}
