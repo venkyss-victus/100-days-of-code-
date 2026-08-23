3a4f5c7cecf7eba93011a6a441f02ea76e7b4b29
VENKY SHARMA <venkycamb@gmail.com>
2026-08-23 15:22:06 +0530

    day6-11 c program

---
+++ day6-11.c
@@ -0,0 +1,16 @@
+//Write a program to input an integer and check whether it is even or odd using if–else.
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is even", num);
+    } else {
+        printf("%d is odd", num);
+    }
+
+    return 0;
+}
