094a943f5918e705ce4f123c26abe88da3c586f6
VENKY SHARMA <venkycamb@gmail.com>
2026-08-23 15:08:18 +0530

    add Day4-7 c program

---
+++ Day4-7.c
@@ -0,0 +1,16 @@
+// Write a program to swap two numbers without using a third variable.
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swap: %d %d", a, b);
+
+    return 0;
+}
