7b36aeb277c8cd03cb5060167e7898dce927cf4c
VENKY SHARMA <venkycamb@gmail.com>
2026-08-23 15:26:35 +0530

    day6-12 c program

---
+++ day6-12.c
@@ -0,0 +1,20 @@
+//Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    scanf("%d", &num);
+
+    if (num >= 0) {
+        if (num == 0) {
+            printf("Zero");
+        } else {
+            printf("Positive");
+        }
+    } else {
+        printf("Negative");
+    }
+
+    return 0;
+}
