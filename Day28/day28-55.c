b2a651b2b4f31c4a72dcf3bea9451f16985dcae4
venky <venky@venkys-MacBook-Air.local>
2026-09-09 14:57:46 +0530

    adding programs

---
+++ Day28/day28-55.c
@@ -0,0 +1,44 @@
+/*Q55: Write a program to print all the prime numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+10
+Output 1:
+2 3 5 7
+
+Input 2:
+20
+Output 2:
+2 3 5 7 11 13 17 19
+
+*/
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are: ", n);
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
